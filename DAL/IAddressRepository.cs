c707f7ae57a7c66fff0f6af94736247fda40d494
RassilonX <chrisfraser18@gmail.com>
2024-06-22 21:04:49 +0100

    Migrated solution into correct repository

---
+++ DAL/IAddressRepository.cs
@@ -0,0 +1,9 @@
+using Database.Models;
+
+namespace DAL;
+
+public interface IAddressRepository
+{
+    Task<Address> GetForPersonIdAsync(int personId);
+    Task SaveAsync(Address address);
+}
---
+++ DAL/IPersonRepository.cs
@@ -0,0 +1,10 @@
+using Database.Models;
+
+namespace DAL;
+
+public interface IPersonRepository
+{
+    Task<List<Person>> ListAllAsync();
+    Task<Person> GetByIdAsync(int personId);
+    Task SaveAsync(Person person);
+}
