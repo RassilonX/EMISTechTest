27ab85e6b116233a63a39ccfe8dc4eeeea1de96f
RassilonX <chrisfraser18@gmail.com>
2024-06-22 21:43:38 +0100

    Set up navigation to and from the import page

---
+++ FullStackTechTest/Controllers/ImportFileController.cs
@@ -0,0 +1,13 @@
+﻿using FullStackTechTest.Models.Home;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackTechTest.Controllers
+{
+    public class ImportFileController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
