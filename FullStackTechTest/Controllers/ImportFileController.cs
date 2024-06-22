520f0e3c45505fe8fb9fc3666f4241747e1db408
RassilonX <chrisfraser18@gmail.com>
2024-06-23 00:40:51 +0100

    Connected the import form to a controller

--- FullStackTechTest/Controllers/ImportFileController.cs
+++ FullStackTechTest/Controllers/ImportFileController.cs
@@ -1,13 +1,33 @@
 ﻿using FullStackTechTest.Models.Home;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
-namespace FullStackTechTest.Controllers
+namespace FullStackTechTest.Controllers;
+
+public class ImportFileController : Controller
 {
-    public class ImportFileController : Controller
+    public async Task<IActionResult> Index()
     {
-        public async Task<IActionResult> Index()
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ImportJson(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
         {
-            return View();
+            return BadRequest("No file was uploaded.");
+        }
+
+        using (var reader = new StreamReader(file.OpenReadStream()))
+        {
+            var jsonData = await reader.ReadToEndAsync();
+
+            dynamic data = JsonConvert.DeserializeObject(jsonData);
+
+            // Store the data in your database or perform any other necessary actions
+
+            return Json(new { success = true });
         }
     }
 }
