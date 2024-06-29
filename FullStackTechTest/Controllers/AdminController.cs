e1d016644972726cd7e2e418cca4d286066b60d0
RassilonX <chrisfraser18@gmail.com>
2024-06-29 21:32:48 +0100

    Added the Admin Page

---
+++ FullStackTechTest/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackTechTest.Controllers;
+
+public class AdminController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+}
